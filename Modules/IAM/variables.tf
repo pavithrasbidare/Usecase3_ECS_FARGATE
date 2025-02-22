bebe3e75fbba681b949d2374e22850dc5b56353a
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-22 20:18:21 +0530

    Create variables.tf

---
+++ Modules/IAM/variables.tf
@@ -0,0 +1,9 @@
+variable "ecs_task_execution_role_name" {
+  description = "Name of the ECS task execution role"
+  type        = string
+}
+
+variable "ecs_service_role_name" {
+  description = "Name of the ECS service role"
+  type        = string
+}
