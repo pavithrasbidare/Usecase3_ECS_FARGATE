137ec0a0e3c9bcc633e893e1cdc66e53d893fbb5
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-22 21:37:38 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "../modules/vpc"
 
   vpc_cidr           = var.vpc_cidr
   vpc_name           = var.vpc_name
@@ -9,14 +9,14 @@ module "vpc" {
 }
 
 module "iam" {
-  source = "./modules/iam"
+  source = "../modules/iam"
 
   ecs_task_execution_role_name = var.ecs_task_execution_role_name
   ecs_service_role_name        = var.ecs_service_role_name
 }
 
 module "ecs" {
-  source = "./modules/ecs"
+  source = "../modules/ecs"
 
   region                     = var.region
   cluster_name               = var.cluster_name
@@ -36,24 +36,14 @@ module "ecs" {
   patient_tg_arn             = var.patient_tg_arn
 }
 
+module "alb" {
+  source      = "../../Modules/ALB"
+  vpc_id      = module.vpc.vpc_id
+  subnets     = module.vpc.public_subnets
+  domain_name = var.domain_name
+}
 
-module "ecs" {
-  source = "./modules/ecs"
-
-  region                     = var.region
-  cluster_name               = var.cluster_name
-  task_family                = var.task_family
-  execution_role             = module.iam.ecs_task_execution_role_arn
-  task_cpu                   = var.task_cpu
-  task_memory                = var.task_memory
-  appointment_container_name = var.appointment_container_name
-  image_url                  = var.image_url
-  patient_container_name     = var.patient_container_name
-  image_url_patient          = var.image_url_patient
-  appointment_service_name   = var.appointment_service_name
-  patient_service_name       = var.patient_service_name
-  subnets                    = module.vpc.public_subnet_ids
-  security_groups            = var.security_groups
-  appointment_tg_arn         = var.appointment_tg_arn
-  patient_tg_arn             = var.patient_tg_arn
+module "monitoring" {
+  source         = "../../Modules/Cloudwatch"
+  log_group_name = var.log_group_name
 }
