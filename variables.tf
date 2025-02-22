fe6ff7c02eb82f1c3c723c2a434d2d7b2fa10dda
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-22 15:11:13 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "public_subnets" {
-  description = "List of public subnets"
-  type        = list(string)
-}
-
-variable "vpc_id" {
-  description = "The VPC ID"
-  type        = string
-}
