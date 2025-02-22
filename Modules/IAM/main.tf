3757812d0acb3d1e69d3d5d62a9c872e42ddd3ca
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-22 20:13:16 +0530

    Update main.tf

--- Modules/IAM/main.tf
+++ Modules/IAM/main.tf
@@ -59,7 +59,32 @@ resource "aws_iam_policy" "cloudwatch_logs_policy" {
   })
 }
 
+resource "aws_iam_policy" "ecr_access_policy" {
+  name        = "ECRAccessPolicy"
+  description = "Policy for allowing ECS tasks to pull images from ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "attach_cloudwatch_logs_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.cloudwatch_logs_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ecr_access_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecr_access_policy.arn
 }
