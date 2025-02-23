f3d47f95e1c4fab042a275b7dd1d6b4e22ba42a8
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 14:05:28 +0530

    Update main.tf

--- Modules/VPC/main.tf
+++ Modules/VPC/main.tf
@@ -1,81 +1,29 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = var.vpc_name
-  }
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "MainInternetGateway"
-  }
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+  default     = "my-vpc"
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "PublicRouteTable"
-  }
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-resource "aws_route_table_association" "public" {
-  count          = length(var.public_subnets)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-resource "aws_subnet" "public" {
-  count = length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnets[count.index]
-  map_public_ip_on_launch = true
-  availability_zone = element(var.availability_zones, count.index)
-
-  tags = { Name = "public-subnet-${count.index}" }
-}
-
-resource "aws_subnet" "private" {
-  count = length(var.private_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnets[count.index]
-  availability_zone = element(var.availability_zones, count.index)
-
-  tags = { Name = "private-subnet-${count.index}" }
-}
-
-resource "aws_eip" "nat" {
-  depends_on = [aws_internet_gateway.main] 
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
-  depends_on    = [aws_internet_gateway.main]
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route" "private_nat_gateway" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
-
-resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+variable "availability_zones" {
+  description = "List of availability zones for subnets"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
 }
