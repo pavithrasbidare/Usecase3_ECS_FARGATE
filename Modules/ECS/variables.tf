858b05295155d70a4e1cbc646acd28176adedc6a
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 15:08:26 +0530

    Update variables.tf

--- Modules/ECS/variables.tf
+++ Modules/ECS/variables.tf
@@ -1,79 +1,93 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
 variable "cluster_name" {
-  description = "ECS cluster name"
+  description = "The name of the ECS cluster"
   type        = string
 }
 
-variable "task_family" {
-  description = "ECS task family"
-  type        = string
-}
-
-variable "execution_role" {
-  description = "IAM role for ECS task execution"
-  type        = string
-}
-
-variable "task_cpu" {
-  description = "Task CPU units"
-  type        = string
-}
-
-variable "task_memory" {
-  description = "Task memory in MiB"
+variable "task_name" {
+  description = "The name of the ECS task definition"
   type        = string
 }
 
 variable "appointment_container_name" {
-  description = "Container name for appointment service"
+  description = "The name of the container"
   type        = string
 }
 
 variable "image_url" {
-  description = "Docker image URL for appointment service"
-  type        = string
-}
-
-variable "patient_container_name" {
-  description = "Container name for patient service"
+  description = "The URL of the Docker image to use in the ECS task"
   type        = string
 }
 
 variable "image_url_patient" {
-  description = "Docker image URL for patient service"
+  description = "The URL of the Docker image to use in the ECS task"
   type        = string
 }
 
-variable "appointment_service_name" {
-  description = "ECS service name for appointment service"
+variable "execution_role" {
+  description = "The execution role for the ECS task"
   type        = string
 }
 
-variable "patient_service_name" {
-  description = "ECS service name for patient service"
+variable "task_cpu" {
+  description = "CPU units for the container"
+  type        = number
+  default     = 256
+}
+
+variable "task_memory" {
+  description = "Memory for the container in MB"
+  type        = number
+  default     = 512
+}
+
+variable "cluster_id" {
+  description = "The ID of the ECS cluster"
   type        = string
 }
 
 variable "subnets" {
-  description = "Subnets for the ECS service"
+  description = "A list of subnet IDs for the ECS service"
   type        = list(string)
 }
 
 variable "security_groups" {
-  description = "Security groups for the ECS service"
+  description = "A list of security group IDs for the ECS service"
   type        = list(string)
 }
 
+variable "desired_count" {
+  description = "The desired number of instances of the ECS service"
+  type        = number
+  default     = 1
+}
+
+variable "task_definition" {
+  description = "The ARN of the ECS task definition"
+  type        = string
+}
+
+variable "patient_container_name" {
+  description = "The name of the container"
+  type        = string
+}
+
+variable "patient_service_name" {
+  description = "The name of the ECS service"
+  type        = string
+}
+
+variable "appointment_service_name" {
+  description = "The name of the ECS service"
+  type        = string
+}
+
 variable "appointment_tg_arn" {
-  description = "Target group ARN for appointment service"
+  description = "Target Group ARN for Appointment Service"
   type        = string
 }
 
 variable "patient_tg_arn" {
-  description = "Target group ARN for patient service"
+  description = "Target Group ARN for Patient Service"
   type        = string
 }
+
