e534f8b0f0a1943cdc1dee90f184d47cdc9157c4
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 18:48:40 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -47,3 +47,23 @@ module "monitoring" {
   source         = "../Modules/Cloudwatch"
   log_group_name = var.log_group_name
 }
+
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.main.id
+}
+
+output "appointment_service_task_definition_arn" {
+  value = aws_ecs_task_definition.appointment_service.arn
+}
+
+output "patient_service_task_definition_arn" {
+  value = aws_ecs_task_definition.patient_service.arn
+}
+
+output "appointment_service_id" {
+  value = aws_ecs_service.appointment_service.id
+}
+
+output "patient_service_id" {
+  value = aws_ecs_service.patient_service.id
+}
