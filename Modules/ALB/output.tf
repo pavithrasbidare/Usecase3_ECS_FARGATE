937c6d9a9d9f8449696cd8270fdb7aaf9a5350ee
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 14:49:35 +0530

    Update output.tf

--- Modules/ALB/output.tf
+++ Modules/ALB/output.tf
@@ -5,3 +5,13 @@ output "dns_name" {
 output "target_group_arn" {
   value = aws_lb_target_group.ecs_tg.arn
 }
+
+output "alb_sg" {
+  description = "Security group for the ALB"
+  value       = aws_security_group.alb_sg.id
+}
+
+output "ecs_tg_arn" {
+  description = "Target group ARN for ECS"
+  value       = aws_lb_target_group.this.arn
+}
