1c6aafda883287cb55e282005f66bdd873df821f
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 19:09:09 +0530

    Update outputs.tf

--- Modules/ECS/outputs.tf
+++ Modules/ECS/outputs.tf
@@ -1,19 +1,9 @@
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.main.id
+output "task_definition_arn" {
+  description = "The ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.appointment_service.arn
 }
 
-output "appointment_service_task_definition_arn" {
-  value = aws_ecs_task_definition.appointment_service.arn
-}
-
-output "patient_service_task_definition_arn" {
-  value = aws_ecs_task_definition.patient_service.arn
-}
-
-output "appointment_service_id" {
-  value = aws_ecs_service.appointment_service.id
-}
-
-output "patient_service_id" {
-  value = aws_ecs_service.patient_service.id
+output "cluster_id" {
+  description = "The ID of the ECS cluster"
+  value       = aws_ecs_cluster.main.id
 }
