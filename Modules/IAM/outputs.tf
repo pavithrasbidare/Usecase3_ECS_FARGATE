6fe12ee1a4b4d4020ff77a15ae7040fc551a4dd8
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 15:09:13 +0530

    Update outputs.tf

--- Modules/IAM/outputs.tf
+++ Modules/IAM/outputs.tf
@@ -1,9 +1,1 @@
-output "ecs_task_execution_role_arn" {
-  description = "ARN of the ECS task execution role"
-  value       = aws_iam_role.ecs_task_execution_role.arn
-}
-
-output "ecs_service_role_arn" {
-  description = "ARN of the ECS service role"
-  value       = aws_iam_role.ecs_service_role.arn
-}
+output "ecs_task_role_arn" { value = aws_iam_role.ecs_task_role.arn }
