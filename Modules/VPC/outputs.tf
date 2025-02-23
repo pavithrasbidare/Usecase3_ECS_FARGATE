104ad0999860268359f057d25467823dcc7dac39
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 14:50:24 +0530

    Update outputs.tf

--- Modules/VPC/outputs.tf
+++ Modules/VPC/outputs.tf
@@ -1,3 +1,21 @@
 output "vpc_id" { value = aws_vpc.main.id }
-output "public_subnets" { value = aws_subnet.public[*].id }
-output "private_subnets" { value = aws_subnet.private[*].id }
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.nat.id
+}
