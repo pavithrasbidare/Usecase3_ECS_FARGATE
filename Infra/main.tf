8e004a5448907b17cb6b7569af6f7858f7e4aa10
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 15:03:16 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source            = "../Modules/VPC"
+  source            = "../../Modules/VPC"
   vpc_cidr         = var.vpc_cidr
   public_subnets   = var.public_subnets
   private_subnets  = var.private_subnets
@@ -7,43 +7,43 @@ module "vpc" {
 }
 
 module "iam" {
-  source = "../Modules/IAM"
-  ecs_service_role_name        = var.ecs_service_role_name
-  ecs_task_execution_role_name = var.ecs_task_execution_role_name
+  source = "../../Modules/IAM"
 }
 
 module "ecr" {
-  source = "../Modules/ECR"
+  source    = "../../Modules/ECR"
+  repo_name = var.repo_name
 }
 
 module "ecs" {
-  source = "../Modules/ECS"
-  region                     = var.region
-  cluster_name               = var.cluster_name
-  task_family                = var.task_family
-  execution_role             = module.iam.ecs_task_execution_role_arn
-  task_cpu                   = var.task_cpu
-  task_memory                = var.task_memory
-  appointment_container_name = var.appointment_container_name
-  image_url                  = module.ecr.appointment_service_repository_url
-  patient_container_name     = var.patient_container_name
-  image_url_patient          = module.ecr.patient_service_repository_url
-  appointment_service_name   = var.appointment_service_name
-  patient_service_name       = var.patient_service_name
-  subnets                    = module.vpc.public_subnets
-  security_groups            = module.alb.alb_sg
-  appointment_tg_arn         = module.alb.ecs_tg_arn
-  patient_tg_arn             = module.alb.ecs_tg_arn
+  source          = "../../Modules/ECS"
+  cluster_name    = var.cluster_name
+  task_name       = var.task_name
+  image_url       = var.image_url
+  image_url_patient = var.image_url_patient
+  task_memory     = var.task_memory
+  task_cpu        = var.task_cpu
+  execution_role  = module.iam.ecs_task_role_arn
+  patient_service_name    = var.patient_service_name
+  appointment_service_name = var.appointment_service_name
+  cluster_id      = module.ecs.cluster_id
+  task_definition = module.ecs.task_definition_arn
+  subnets         = module.vpc.private_subnets
+  security_groups = module.alb.sg_id
+  appointment_container_name  = var.appointment_container_name
+  patient_container_name = var.patient_container_name
+  appointment_tg_arn  = module.alb.appointment_tg_arn
+  patient_tg_arn      = module.alb.patient_tg_arn
 }
 
 module "alb" {
-  source      = "../Modules/ALB"
+  source      = "../../Modules/ALB"
   vpc_id      = module.vpc.vpc_id
   subnets     = module.vpc.public_subnets
   domain_name = var.domain_name
 }
 
 module "monitoring" {
-  source         = "../Modules/Cloudwatch"
+  source         = "../../Modules/Cloudwatch"
   log_group_name = var.log_group_name
 }
