add68035c466e0bd7b2c9c538c39c2e106f76e2f
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 14:37:41 +0530

    Update variables.tf

--- Modules/ALB/variables.tf
+++ Modules/ALB/variables.tf
@@ -1,9 +1,26 @@
-variable "public_subnets" {
-  description = "List of public subnets"
+variable "vpc_id" {
+  description = "The ID of the VPC where the ALB and target groups will be deployed"
+  type        = string
+}
+
+variable "subnets" {
+  description = "List of subnet IDs where the ALB will be deployed"
   type        = list(string)
 }
 
-variable "vpc_id" {
-  description = "The VPC ID"
+variable "alb_name" {
+  description = "The name of the Application Load Balancer"
   type        = string
+  default  = "ecs_alb"
+}
+
+variable "domain_name" {
+  description = "The domain name for the ACM certificate"
+  type        = string
+}
+
+variable "alb_security_group_name" {
+  description = "Name of the security group for ALB"
+  type        = string
+  default     = "alb-sg"
 }
