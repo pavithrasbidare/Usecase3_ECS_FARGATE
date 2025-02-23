7216d4b04b5ed8c80bbb044213bb1326ada92691
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-23 15:07:16 +0530

    Update output.tf

--- Modules/ECR/output.tf
+++ Modules/ECR/output.tf
@@ -1,7 +1,1 @@
-output "patient_service_repository_url" {
-  value = aws_ecr_repository.patient_service.repository_url
-}
-
-output "appointment_service_repository_url" {
-  value = aws_ecr_repository.appointment_service.repository_url
-}
+output "repository_url" { value = aws_ecr_repository.app_repo.repository_url }
